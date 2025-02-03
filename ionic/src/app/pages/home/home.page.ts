db8e7194c1f480383064bf650db51d893c2fa47c
Lucas Von Ryn <lvonryn@gmail.com>
2025-02-03 20:49:41 -0300

    fix: ajustes de roteamento

---
+++ ionic/src/app/pages/home/home.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  imports: [IonicModule, CommonModule],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
