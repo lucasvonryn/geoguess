f8f689246536e5f5f726e0d1a0054345eb76efa8
Lucas Von Ryn <lvonryn@gmail.com>
2025-02-03 20:53:58 -0300

    feat: defini a estrutura de pastas das telas do jogo

--- ionic/src/app/app-routing.module.ts
+++ ionic/src/app/app-routing.module.ts
@@ -15,6 +15,22 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
+  {
+    path: 'report',
+    loadChildren: () => import('./pages/report/report.module').then( m => m.ReportPageModule)
+  },
+  {
+    path: 'about',
+    loadChildren: () => import('./pages/about/about.module').then( m => m.AboutPageModule)
+  },
+  {
+    path: 'credits',
+    loadChildren: () => import('./pages/credits/credits.module').then( m => m.CreditsPageModule)
+  },
+  {
+    path: 'game',
+    loadChildren: () => import('./pages/game/game.module').then( m => m.GamePageModule)
+  },
 ];
 
 @NgModule({
